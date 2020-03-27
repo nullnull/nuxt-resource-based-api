e0f389fa45a6026f7c1f28bafd9c2505bc5c9b47
Katsuma Narisawa <face.the.music721@gmail.com>
2020-03-27 16:37:35 +0900

    Update test for defaultQueryHandler

--- test/default/query_handler.test.ts
+++ test/default/query_handler.test.ts
@@ -1,25 +1,97 @@
 import queryHandler from './../../src/default/queryHandler'
 
-function sharedTest(resource, action, path, expected) {
-  test('with no namespace', () => {
-    const context = {
-      query: {},
-      route: {
-        params: { id: 1 },
-        path: path
-      },
-      
-    }
+function sharedTest(resource, action, context, expected) {
+  test('it extracts valid queries from path', () => {
     const query = queryHandler(resource, action, context)
     expect(query).toEqual(expected)
   })
 }
 
-describe('actions of createStore', () => {
-  sharedTest('sharedArticle', 'show', '/shared_articles/1', { id: 1 })
-  sharedTest('sharedArticle', 'show', '/shared_articles/1/comments/2', { id: 1 })
-  sharedTest('sharedArticle', 'show', '/users/2/shared_articles/1', { id: 1 })
+describe('default query handler', () => {
+  describe('index action', () => {
+    sharedTest('user', 'index', {
+      query: {},
+      route: {
+        params: {},
+        path: '/users'
+      },
+    }, {})
 
-  sharedTest('sharedArticle', 'edit', '/shared_articles/1/edit', { id: 1 })
-  sharedTest('sharedArticle', 'edit', '/shared_articles/1/edit?foo=2', { id: 1 })
+    sharedTest('comment', 'index', {
+      query: {},
+      route: {
+        params: { userId: 1 },
+        path: '/users/1/comments'
+      },
+    }, { userId: 1 })
+  })
+
+  describe('show action', () => {
+    sharedTest('user', 'show', {
+      query: {},
+      route: {
+        params: { id: 1 },
+        path: '/users/1'
+      },
+    }, { id: 1 })
+
+    sharedTest('user', 'show', {
+      query: {},
+      route: {
+        params: { userId: 1 },
+        path: '/users/1'
+      },
+    }, { id: 1 })
+
+    sharedTest('user', 'show', {
+      query: {},
+      route: {
+        params: {
+          userId: 1,
+          id: 2,
+        },
+        path: '/users/1/comments/2',
+      },
+    }, { id: 1 })
+
+    sharedTest('comment', 'show', {
+      query: {},
+      route: {
+        params: {
+          userId: 1,
+          id: 2,
+        },
+        path: '/users/1/comments/2',
+      },
+    }, { id: 2 })
+  })
+
+  describe('edit action', () => {
+    sharedTest('user', 'edit', {
+      query: {},
+      route: {
+        params: { id: 1 },
+        path: '/users/1/edit'
+      },
+    }, { id: 1 })
+
+    sharedTest('user', 'edit', {
+      query: {},
+      route: {
+        params: { userId: 1 },
+        path: '/users/1/edit'
+      },
+    }, { id: 1 })
+
+    sharedTest('comment', 'edit', {
+      query: {},
+      route: {
+        params: {
+          userId: 1,
+          id: 2,
+        },
+        path: '/users/1/comments/2/edit',
+      },
+    }, { id: 2 })
+  })
 })
